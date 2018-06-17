aa63b8d04d1caa3162dd343743da9cbc8e8d7db4
Joseph Parker <jparker010190@gmail.com>
2018-06-17 03:51:43 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,7 @@ class StudentsController < ApplicationController
 
   def create
   Student.create(first_name: params[:first_name], last_name: params[:last_name])
-  redirect_to student_path
+  redirect_to student_path(student)
 end
 
 end
